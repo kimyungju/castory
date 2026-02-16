e2366f4b3e8d39dd58b9ec74a8975ff7a24a5e7e
Kim Yungju <yjkim101002@gmail.com>
2026-02-17 03:23:53 +0800

    fix: add Log Out button and a11y fixes to MobileNav

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -2,18 +2,23 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { cn, normalizeImageSrc } from "@/lib/utils";
 import { sidebarLinks } from "@/constants";
+import { SignedIn, useClerk } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
 import {
   Sheet,
   SheetClose,
   SheetContent,
+  SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
 
 const MobileNav = () => {
   const pathname = usePathname();
+  const router = useRouter();
+  const { signOut } = useClerk();
 
   return (
     <Sheet>
@@ -26,7 +31,8 @@ const MobileNav = () => {
           className="cursor-pointer"
         />
       </SheetTrigger>
-      <SheetContent side="left" className="border-none bg-black-1">
+      <SheetContent side="left" className="border-none bg-black-1" aria-describedby={undefined}>
+        <SheetTitle className="sr-only">Navigation Menu</SheetTitle>
         <Link href="/" className="flex cursor-pointer items-center gap-1 pb-10">
           <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
           <h1 className="text-24 font-extrabold text-white-1">Castory</h1>
@@ -63,6 +69,17 @@ const MobileNav = () => {
               })}
             </nav>
           </SheetClose>
+
+          <SignedIn>
+            <div className="flex-center w-full pb-14">
+              <Button
+                className="text-16 w-full bg-orange-1 font-extrabold"
+                onClick={() => signOut(() => router.push("/sign-in"))}
+              >
+                Log Out
+              </Button>
+            </div>
+          </SignedIn>
         </div>
       </SheetContent>
     </Sheet>
